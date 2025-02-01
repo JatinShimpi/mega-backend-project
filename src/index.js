e90bd47bc8945ce0d8e1aba4ffab4fab12f3d143
JatinShimpi <shimpijatin184@gmail.com>
2025-02-01 15:23:15 +0530

    added util code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running on port ${process.env.PORT}`);
+    });
+    
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed !!!", err);
+  });
 
 /*
 import express from "express"
