ff1251446f8b307a7f647ad47a976cadc361e4be
JatinShimpi <shimpijatin184@gmail.com>
2025-02-15 16:51:04 +0530

    add healthcheck and dashboard routes, implement tweet, comment, like, and playlist models, and fix refresh token typo in user model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -34,7 +34,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUSer);
 router.route("/update-account").patch(verifyJWT, updateAccounddetails);
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
